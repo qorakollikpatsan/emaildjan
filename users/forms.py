7ed22cea24c34896a79c50dfa2a72e9908ad8176
2025 <sh@gmail.com>
2026-03-18 12:45:48 +0500

    commit

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,5 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from sms.views import register
-
+from .views import register
+from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
 ]
